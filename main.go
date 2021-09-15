e8e4da3dd3a14b2b2bcbd271530ea5729725be76
tinker <gaoqi198@163.com>
2021-09-15 23:51:30 +0800

    feat: resolve conf files

--- main.go
+++ main.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"finance/cls"
 	util "finance/utils"
 	"fmt"
 	"math/rand"
 	"time"
+	"io/ioutil"
 )
 
-func main() {
+var conf *Conf
 
+type HotPlate struct {
+	Host string `json:"host"`
+	Path string `json:"path"`
+}
+
+type NewsPlate struct {
+	Host string `json:"host"`
+	Path string `json:"path"`
+}
+
+type ClsInfo struct {
+	Hot HotPlate `json:"hot"`
+	News NewsPlate `json:"news"`
+}
+
+type Conf struct {
+	Webhook string `json:"webhook"`
+	Cls ClsInfo `json:"cls"`
+}
+
+func init()  {
+	filePath := "./conf/dev.json"
+
+	var (
+		data []byte
+		err error
+	)
+	data, err = ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("read conf file err", err)
+		return
+	}
+
+	er := json.Unmarshal(data, &conf)
+	if er != nil {
+		fmt.Println("json unMarsha1 conf file err", er)
+		return
+	}
+}
+
+func main() {
 	ch := make(chan int64, 1)
 	ch <- time.Now().Unix()
 
