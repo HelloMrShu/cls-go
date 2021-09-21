63abb20c8c5c88f942c17cdafccaebea96ba224f
tinker <gaoqi198@163.com>
2021-09-21 22:55:17 +0800

    feat: import logrus

--- main.go
+++ main.go
@@ -4,21 +4,28 @@ import (
 	"finance/app"
 	"finance/cls"
 	"flag"
+	"github.com/sirupsen/logrus"
+	"os"
 	"strings"
 	"time"
 )
 
-var (
-	env string
-)
+var env string
+var log = logrus.New()
 
 func init() {
 	flag.StringVar(&env, "c", "local", "conf path")
 	flag.Parse()
 	config := "./conf/" + env + ".json"
-
 	app.NewCycle(config)
 
+	//设置output,默认为stderr,可以为任何io.Writer，比如文件*os.File
+	log.SetFormatter(&logrus.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	file, _ := os.OpenFile("./logs/app.log", os.O_CREATE|os.O_WRONLY, 0666)
+	log.SetOutput(file)
+	//设置最低loglevel
+	log.SetLevel(logrus.InfoLevel)
 }
 
 func main() {
