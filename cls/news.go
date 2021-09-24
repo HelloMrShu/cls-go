237a49b49bfdb8d640bc21b9dedd28f6d01c2c71
SOHU-INC\qigao216538 <986934994@qq.com>
2021-09-24 17:24:28 +0800

    feat: 日志

--- cls/news.go
+++ cls/news.go
@@ -109,7 +109,7 @@ func NewsRequest(lt int64) int64 {
 		return time.Now().Unix()
 	}
 
-	fmt.Println("新闻消息推送条数：", updateNum)
+	app.Logger.Info("新闻消息推送条数: " + string(updateNum))
 	newTs := time.Now().Unix()
 	for _, v := range news {
 		msgInfo := GenNewsMessage(v.Brief)
