f3aed62b402f5f406072eb1b946254c4b47c9a8a
SOHU-INC\qigao216538 <986934994@qq.com>
2021-09-24 16:09:13 +0800

    feat: logger 异常处理

--- app/logger.go
+++ app/logger.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"os"
 )
@@ -11,7 +12,21 @@ func InitLogger() {
 	Logger.SetReportCaller(true)
 	Logger.SetFormatter(&LogFormatter{})
 	Logger.SetOutput(os.Stdout)
-	file, _ := os.OpenFile("./logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0755)
+
+	logPath := "./logs/app.log"
+	_, err := os.Stat(logPath)
+	if err != nil {
+		ret := os.MkdirAll(logPath, 0755)
+		if ret != nil {
+			fmt.Println("can't create file ", ret)
+			return
+		}
+	}
+
+	file, err := os.OpenFile("", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0755)
+	if err != nil {
+		fmt.Println("open ", err)
+	}
 	Logger.SetOutput(file)
 	Logger.SetLevel(logrus.InfoLevel)
 }
